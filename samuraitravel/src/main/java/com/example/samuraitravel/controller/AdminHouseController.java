eb98212b4821c0e2c45e0aeca5448ad2c2a311da
kinok <kinok@DESKTOP-09A47AU>
2025-06-10 14:08:54 +0900

    ホテルル作成(06/09)_14:00

--- samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -8,7 +8,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.repository.HouseRepository;
@@ -25,15 +27,38 @@ public class AdminHouseController {
 //		this.houseRepository = houseRepository;
 //	}
 	
+//	@GetMapping
+//	public String index(Model model, Pageable pageable) { //Pageable型引数を指定することで、Pageableオブジェクトを生成し、メソッド内で利用可能となる。
+//		List<House> houses = houseRepository.findAll(); //HouseRepositoryインターフェイスのfindByAll()メソッドで全てぼ民宿データを取得する
+//		model.addAttribute("houses", houses); //viewへ渡す
+//		return "admin/houses/index";
+//	}
+	
+	//GETリクエスト(/admin/houses)を受取、民宿一覧及び検索、登録画面を返す
 	@GetMapping
-	//public String index(Model model, Pageable pageable) { //Pageable型引数を指定することで、Pageableオブジェクトを生成し、メソッド内で利用可能となる。
-	public String index(Model model, @PageableDefault(page=0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {
-		//List<House> houses = houseRepository.findAll(); //HouseRepositoryインターフェイスのfindByAll()メソッドで全てぼ民宿データを取得する
-		Page<House> housePages = houseRepository.findAll(pageable);
+	public String index(Model model, @PageableDefault(page=0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, @RequestParam(name = "keyword", required = false) String keyword) {
 		
-		//model.addAttribute("houses", houses); //viewへ渡す
-		model.addAttribute("housePage", housePages);
+		Page<House> housePage = houseRepository.findAll(pageable);//ページの全件取得
+		
+		if(keyword != null && !keyword.isEmpty()) { //keywordパラメータが存在するかどうか
+			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable); //存在すれば検索/取得してhousePageに格納
+		}else {
+			housePage = houseRepository.findAll(pageable); //無ければ全件取得
+		}
+		
+		model.addAttribute("housePage", housePage);
+		model.addAttribute("keyword",keyword);
 		
 		return "admin/houses/index";
 	}
+	
+	//一覧ページから/{id}を受取、各民宿の詳細ページを返す
+	@GetMapping("/{id}")
+	public String show(@PathVariable(name = "id") Integer id, Model model) { //@PathVariable:name属性にバインドさせたいURLの{}内の文字列を指定。
+		House house = houseRepository.getReferenceById(id); //getReferenceById(id):引数idと一致するデータを取得する
+		model.addAttribute("house", house);
+		
+		return "admin/houses/show";
+	}
+	
 }
--- samuraitravel/src/main/java/com/example/samuraitravel/repository/HouseRepository.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/repository/HouseRepository.java
@@ -1,10 +1,13 @@
 package com.example.samuraitravel.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.samuraitravel.entity.House;
 
 //②宿泊施設用DB接続Repository
 public interface HouseRepository extends JpaRepository<House, Integer>{ //エンティティのクラス型と、主キーのデータ型を指定
-
+	
+	public Page<House> findByNameLike(String keyword, Pageable pageable);
 }
