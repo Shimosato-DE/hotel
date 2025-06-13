1e2ca37b33c38f1b081e23012a995c6e65924315
kinok <kinok@DESKTOP-09A47AU>
2025-06-13 15:44:16 +0900

    ホテルル作成(06/13)_16:00

--- samuraitravel/src/main/java/com/example/samuraitravel/repository/HouseRepository.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/repository/HouseRepository.java
@@ -1,5 +1,7 @@
 package com.example.samuraitravel.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,19 +11,22 @@ import com.example.samuraitravel.entity.House;
 //②宿泊施設用DB接続Repository
 public interface HouseRepository extends JpaRepository<House, Integer>{ //エンティティのクラス型と、主キーのデータ型を指定
 	
+	
 	public Page<House> findByNameLike(String keyword, Pageable pageable);
 	
-	public Page<House> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);
-		
 	public Page<House> findByAddressLike(String area, Pageable pageable);
 	
-	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
+	public Page<House> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);
 	
-	public Page<House> findByNameLikeOrAddressLikeOrderByCreateAtDesc(String nameKeyword, String addressKeyword, Pageable pageable);
+	public Page<House> findByNameLikeOrAddressLikeOrderByCreatedAtDesc(String nameKeyword, String addressKeyword, Pageable pageable);
 	
-	public Page<House> findByNameLikeOrAddressLikeOrderByPriceAsc(String nameKeyword, String AddressKeyword, Pageable pageable);
+	public Page<House> findByNameLikeOrAddressLikeOrderByPriceAsc(String nameKeyword, String addressKeyword, Pageable pageable);
 	
 	public Page<House> findByAddressLikeOrderByCreatedAtDesc(String area, Pageable pageable);
+	
+	public Page<House> findByAddressLikeOrderByPriceAsc(String area, Pageable pageable);
+	
+	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
 	
 	public Page<House> findBypriceLessThanEqualOrderByCreatedAtDesc(Integer price, Pageable pageable);
 	
@@ -30,5 +35,7 @@ public interface HouseRepository extends JpaRepository<House, Integer>{ //エン
 	public Page<House> findAllByOrderByCreatedAtDesc(Pageable pageable);
 	
 	public Page<House> findAllByOrderByPriceAsc(Pageable pageable);
+	
+	public List<House> findTop10ByOrderByCreatedAtDesc();
 	
 }
